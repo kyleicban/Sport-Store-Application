0da3f315f1d0c543e0360428a6969cd4b05a9d98
Kyle Icban <kyleicban@yahoo.com>
2020-08-25 14:06:33 -0700

    Up to pg 112 of Pro React 16 (ch5)

---
+++ src/shop/Shop.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { CategoryNavigation } from "./CategoryNavigation";
+import { ProductList } from "./ProductList";
+
+export class Shop extends Component {
+  render() {
+    return (
+      <div className="containuer-fluid">
+        <div className="row">
+          <div className="col bg-dark text-white">
+            <div className="navbar-brand">SPORTS STORE</div>
+          </div>
+        </div>
+        <div className="row">
+          <CategoryNavigation
+            baseUrl="/shops/products"
+            categories={this.props.categories}
+          />
+        </div>
+        <div className="col-9 p-2">
+          <ProductList products={this.props.products} />
+        </div>
+      </div>
+    );
+  }
+}
