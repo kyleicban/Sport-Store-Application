a32c3c82e8b7e99f37396f2746437fbebd6565c6
Kyle Icban <kyleicban@yahoo.com>
2020-08-27 22:13:44 -0700

    Added REST and checkout, finishing ch6

--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -1,71 +1,68 @@
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import { loadData } from "../data/ActionCreators";
+import * as ShopActions from "../data/ActionCreators";
 import { DataTypes } from "../data/Types";
 import { Shop } from "./Shop";
-import {
-  addToCart,
-  updateCartQuantity,
-  removeFromCart,
-  clearCart,
-} from "../data/CartActionCreators";
+import * as CartActions from "../data/CartActionCreators";
 import { CartDetails } from "./CartDetails";
+import { DataGetter } from "../data/DataGetter";
+import { Checkout } from "./Checkout";
+import { Thanks } from "./Thanks";
 
-const mapStateToProps = (dataStore) => ({
-  ...dataStore,
-});
+const mapDispatchToProps = { ...ShopActions, ...CartActions };
 
-const mapDispatchToProps = {
-  loadData,
-  addToCart,
-  updateCartQuantity,
-  removeFromCart,
-  clearCart,
-};
-
-const filterProducts = (products = [], category) =>
-  !category || category === "All"
-    ? products
-    : products.filter(
-        (p) => p.category.toLowerCase() === category.toLowerCase()
-      );
+//const filterProducts = (products = [], category) =>
+//!category || category === "All"
+//? products
+//: products.filter(
+//  (p) => p.category.toLowerCase() === category.toLowerCase()
+//);
 
 export const ShopConnector = connect(
-  mapStateToProps,
+  (ds) => ds,
   mapDispatchToProps
 )(
   class extends Component {
+    selectComponent = (routeProps) => {
+      const wrap = (Component, Content) => (
+        <Component {...this.props} {...routeProps}>
+          {Content && wrap(Content)}
+        </Component>
+      );
+      switch (routeProps.match.params.section) {
+        case "products":
+          return wrap(DataGetter, Shop);
+        case "cart":
+          return wrap(CartDetails);
+        case "checkout":
+          return wrap(Checkout);
+        case "thanks":
+          return wrap(Thanks);
+        default:
+          return <Redirect to="/shop/products/all/1" />;
+      }
+    };
+
     render() {
       return (
         <Switch>
-          <Route
-            path="/shop/products/:category?"
-            render={(routeProps) => (
-              <Shop
-                {...this.props}
-                {...routeProps}
-                products={filterProducts(
-                  this.props.products,
-                  routeProps.match.params.category
-                )}
-              />
-            )}
+          <Redirect
+            from="/shop/products/:category"
+            to="/shop/products/:category/1"
+            exact={true}
           />
           <Route
-            path="/shop/cart"
-            render={(routeProps) => (
-              <CartDetails {...this.props} {...routeProps} />
-            )}
+            path={"/shop/:section?/:category?/:page?"}
+            render={(routeProps) => this.selectComponent(routeProps)}
           />
-          <Redirect to="/shop/products" />
         </Switch>
       );
     }
 
-    componentDidMount() {
+    componentDidMount = () => {
       this.props.loadData(DataTypes.CATEGORIES);
-      this.props.loadData(DataTypes.PRODUCTS);
-    }
+      //this.props.loadData(DataTypes.PRODUCTS);
+    };
   }
 );
